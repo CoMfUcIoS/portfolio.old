dc7bcee271dda963cbb786a14014cd2107d5be12
Ioannis Karasavvaidis <john@studio110.eu>
2016-05-02 22:35:09 +0100

    fixed the loader :D

---
+++ client/footer/helpers.js
@@ -0,0 +1,12 @@
+Template.footer.helpers({
+  "invokeAfterLoad": function() {
+    Meteor.defer(function () {
+      setTimeout(function() {
+        $(".loader .inner").fadeOut(500, function() {
+          $(".loader").fadeOut(750);
+        });
+      }, 3000);
+    });
+    return "";
+  }  
+});
--- client/footer/onRendered.js
+++ client/footer/onRendered.js
@@ -1,8 +1,2 @@
 Template.footer.onRendered(function() {
-	setTimeout(function(){
-	  $(".loader .inner").fadeOut(500, function() {
-	    $(".loader").fadeOut(750);
-	  });
-		
-	}, 3000);
 });
